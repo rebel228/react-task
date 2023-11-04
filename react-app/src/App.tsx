29e7176f05cdd4f54fdb3d4d9ba815eb8212d1d8
Novik Dmytro <dinovik@rambler.ru>
2023-11-04 15:55:13 +0300

    refactor: convert app.tsx to functional component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,13 @@
-import { Component } from "react";
 import "./App.css";
 import PokemonsSearch from "./components/PokemonSearch/pokemonsSearch";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
-export default class App extends Component {
-  render() {
-    return (
-      <main>
-        <ErrorBoundary>
-          <PokemonsSearch />
-        </ErrorBoundary>
-      </main>
-    );
-  }
+export default function App() {
+  return (
+    <main>
+      <ErrorBoundary>
+        <PokemonsSearch />
+      </ErrorBoundary>
+    </main>
+  );
 }
