573702bc71d78ea1bac0a502b1443102fa766ac1
Novik Dmytro <dinovik@rambler.ru>
2023-11-06 02:42:11 +0300

    fix: fix a bug with wrong case

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import PokemonsSearch from "./components/PokemonSearch/PokemonsSearch";
+import PokemonsSearch from "./components/PokemonSearch/pokemonsSearch";
 
 export default function App() {
   return (
