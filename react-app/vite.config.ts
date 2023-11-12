96e2752eab53c72907d3a3282a590dce88b86758
Novik Dmytro <dinovik@rambler.ru>
2023-11-12 17:10:56 +0300

    feat: add testing setup

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +8,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+  },
 });
