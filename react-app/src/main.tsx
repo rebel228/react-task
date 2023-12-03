165c4cf154092474aa160541133e51912c97fe15
Novik Dmytro <dinovik@rambler.ru>
2023-12-03 12:12:53 +0300

    feat: add a router

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const DEFAULT_PATH = "/";
+
+const router = createBrowserRouter([
+  {
+    path: DEFAULT_PATH,
+    element: <App />,
+    children: [
+      {
+        path: `/formone`,
+        element: <div />,
+      },
+      {
+        path: `/formtwo`,
+        element: <div />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
